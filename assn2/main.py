27377b6efdfd4bbe288d6b223246064ee0b3977c
ejb1733 <ethanjbillings@gmail.com>
2024-11-01 15:12:06 -0700

    implement plottr()

--- assn2/main.py
+++ assn2/main.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from science.ODEs import ODEs_T
+from science.ODEs import ODEs_T, eruption_year
 from utilities.solvers import rk4 
 from utilities.plotters import plottr
+from data.constants import *
 
 # T0s = np.zeros(6)
 
@@ -18,10 +19,11 @@ delta_x_yrs = delta_x / 60 / 60 / 24 / 365
 
 outps = rk4(fxy=ODEs_T, x0=x0, xf=xf, y0=T0s, N=N)
 
-plot = plottr(outps[0], outps[1], 
-              xlab='time (s)', ylab='temprature (K)',
-              title=f'temp over time for x0={x0}, xf={xf}, h={(xf-x0)/N}',
-              suptitle=f'Temperature vs Time')
+plot = plottr(outps[0]/YEARS_TO_SECONDS, outps[1]-273.15, 
+              xlab='time (yrs)', ylab='temprature (\xb0C)',
+              title=f'emissivity $\u03B5 = {EPSILON}$',
+              suptitle=f'Temperature Evolution of Six Zones',
+              xline=eruption_year)
 
 print(outps[1][len(outps[1])-1])
 print(f'years integrated over: {delta_x_yrs}')
