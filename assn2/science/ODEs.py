fa10a03e99a18e9298df6dcca2b83f1f5d3a3f73
ejb1733 <ethanjbillings@gmail.com>
2024-11-01 23:12:05 -0700

    complete ODEs documentation

--- assn2/science/ODEs.py
+++ assn2/science/ODEs.py
@@ -1,9 +1,11 @@
 import numpy as np
 
-from .forcings import solarflux, phi_volcano, albedo_t
+from .forcings import phi_volcano, albedo_t
 from data.constants import *
 
 okwtf = 1
+
+# set year at which eruptions will occur
 eruption_year = 25
 
 # Define function for returning n ODEs in accordance with our temperature model
@@ -22,28 +24,24 @@ def ODEs_T(t,T, VOLC=True, ALBEDO_TEMP_DEPENDENT=True):
     n = len(T)
     ODEs = np.zeros(n)
 
+    # set default for volcanism forcing (1 == no volcanism)
     phi = 1
-    # if VOLC:
-    #     phi = phi_volcano(t,year=eruption_year)
 
-    # print(f'GAMMAS: {GAMMAS}, ALBEDOS AVGS: {albedo_avgs}')
-
+    # ODEs for zone 1
     ODEs[0] = (1/PCZ_AVGS[0]) * (GAMMAS[0] * (1-ALBEDO_SKYS[0])*(1-albedo_t(ALBEDO_AVGS[0],ALBEDO_ICE,abs(T[0]),ALBEDO_TEMP_DEPENDENT))*SOLAR_CONST - EPSILON*TAU*SIGMA_B*T[0]**4) + thermal_exchange_rates[0]/(ZONE_SAREAS[0]*PCZ_AVGS[0])*(T[1]-T[0])
 
+    # set volcano to act only in zone 3
     for r in range(1,n-1):
-
         if VOLC:
             if (r == 3):
                 phi = phi_volcano(t, year=eruption_year)
             else:
                 phi = 1
         
-        # calculate temperature at time t for each ODE
+        # ODEs for zones 2-5
         ODEs[r] = (1/PCZ_AVGS[r]) * (GAMMAS[r] * (1-ALBEDO_SKYS[r])*(1-albedo_t(ALBEDO_AVGS[r],ALBEDO_ICE,abs(T[r]),ALBEDO_TEMP_DEPENDENT))*phi*SOLAR_CONST - EPSILON*TAU*SIGMA_B*T[r]**4) + 1/(ZONE_SAREAS[r]*PCZ_AVGS[r])*(-thermal_exchange_rates[r-1]*(T[r]-T[r-1]) + thermal_exchange_rates[r]*(T[r+1]-T[r]))
 
+    # ODEs for zone 6
     ODEs[5] = (1/PCZ_AVGS[5]) * (GAMMAS[5] * (1-ALBEDO_SKYS[5])*(1-albedo_t(ALBEDO_AVGS[5],ALBEDO_ICE,abs(T[5]),ALBEDO_TEMP_DEPENDENT))*SOLAR_CONST - EPSILON*TAU*SIGMA_B*T[5]**4) - thermal_exchange_rates[4]/(ZONE_SAREAS[5]*PCZ_AVGS[5])*(T[5]-T[4])
 
     return ODEs
-
-out = ODEs_T(0,np.zeros(6))
-print(f'T=0s:   {out}')
